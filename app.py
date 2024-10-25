f93f362074c6d43493b6f24ba037286492b34c8a
garv <garvs0109@gmail.com>
2024-10-25 22:58:16 +0530

    chenges

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from pprint import pprint
+from flask import Flask, render_template, request, jsonify, session
 import google.generativeai as genai, os, markdown2
 
 app = Flask(__name__)
+app.secret_key = 'flask-insecure-ot%!h9epy)g9scdb^$)ymmt&#@ca=pyjg+7-p_yh89di6g0^c5'
 
 API_KEY = 'AIzaSyB2kJq_QfzrLx3qyc_50o1QtzkseukiXXk'
 genai.configure(api_key=API_KEY)
@@ -16,16 +18,16 @@ def get_gemini_response(prompt):
     global chat
     response = chat.send_message(prompt)
     resp = response.text
+    resp = markdown2.markdown(resp)
 
     history.append({'role': 'user', 'parts': prompt})
     history.append({'role': 'model', 'parts': resp})
-
-    resp = markdown2.markdown(resp)
+    
     return resp
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', chat_history=history)
 
 @app.route('/get-response', methods=['POST'])
 def get_response():
@@ -35,40 +37,3 @@ def get_response():
 
 if app.name == '__main__':
     app.run(debug=True)
-from flask import Flask, render_template, request, jsonify
-import google.generativeai as genai, os, markdown2
-
-app = Flask(__name__)
-
-API_KEY = 'AIzaSyB2kJq_QfzrLx3qyc_50o1QtzkseukiXXk'
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
-history=[
-    {'role':'user', 'parts':'Hello'},
-    {'role':'model', 'parts':"Great to meet you. What would you like to know?"}
-]
-chat = model.start_chat(history=history)
-
-def get_gemini_response(prompt):
-    global chat
-    response = chat.send_message(prompt)
-    resp = response.text
-
-    history.append({'role': 'user', 'parts': prompt})
-    history.append({'role': 'model', 'parts': resp})
-
-    resp = markdown2.markdown(resp)
-    return resp
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/get-response', methods=['POST'])
-def get_response():
-    user_input = request.form['user_input'].strip()
-    response = get_gemini_response(user_input)
-    return jsonify({'chatbot_response': response, 'user_input': user_input})
-
-if app.name == '__main__':
-    app.run()
